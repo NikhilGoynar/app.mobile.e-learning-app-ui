711b1bdbd9582ffdaa08f493ae733097cb0c7467
Nikhil Goyner <goynernikhil@gmail.com>
2022-12-15 17:08:23 +0530

    OK

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
+import 'package:e_learnig_clone/pages/home_page.dart';
+import 'package:e_learnig_clone/pages/welcome_page.dart';
 import 'package:e_learnig_clone/root_app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-      theme: ThemeData(fontFamily: 'WorkSans'),
-      debugShowCheckedModeBanner: false,
-      home: RootApp(),
-    ));
+    theme: ThemeData(fontFamily: 'WorkSans'),
+    debugShowCheckedModeBanner: false,
+    home: RootApp(),
+  ));
 }
-
-
